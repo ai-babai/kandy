71d66c203b5c3e7949e3599b843854675093b124
maxim-popkov <maxim.popkov.91@gmail.com>
2024-03-27 23:43:07 +0300

    ADD example and png generated image

---
+++ examples/example.py
@@ -0,0 +1,15 @@
+from kandy import Kandy
+
+kandy = Kandy(
+    api_key="your_api_key", 
+    secret_key="your_secret_key"
+)
+
+images = kandy.images.generate(
+    pos_prompt="pet animals",
+    neg_prompt="dog"
+)
+
+if images:
+    image = images[0].to_image()
+    image.save("generated_image.png")
